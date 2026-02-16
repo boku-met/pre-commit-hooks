171624fc8e172082dc4139253b55e49959d0ac26
jan <152862650+j-haacker@users.noreply.github.com>
2026-02-16 22:32:39 +0100

    Fix

--- src/pre_commit_hooks/sync_readme.py
+++ src/pre_commit_hooks/sync_readme.py
@@ -1,8 +1,13 @@
 from pathlib import Path
+from sys import exit
 
-src=Path("README.md")
-dst=Path("docs") / "README.md"
+src = Path("README.md")
+dst = Path("docs") / "README.md"
 dst.parent.mkdir(parents=True, exist_ok=True)
-data=src.read_bytes()
-old=dst.read_bytes() if dst.exists() else None
-(old!=data) and dst.write_bytes(data)
+data = src.read_bytes()
+old = dst.read_bytes() if dst.exists() else None
+if old != data:
+    dst.write_bytes(data)
+    exit(1)
+else:
+    exit(0)
